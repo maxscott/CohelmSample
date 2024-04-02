b1d0d9f0d5914ffa76baf7933dd11f4cb987c47a
Max Weinbrown <max.weinbrown@gmail.com>
2024-04-02 14:36:02 -0400

    bugfix for shutdown/restart

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,7 @@ case_runner = container["case_runner"]
 @app.on_event("startup")
 async def on_startup():
     case_runner.start()
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    case_runner.stop()
